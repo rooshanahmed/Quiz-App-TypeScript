f954779e2e979f1c1516d3f72faae0288e5ed1de
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-07-27 01:15:57 +0500

    App Updated

---
+++ src/styles/App.Styles.ts
@@ -0,0 +1,14 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+    html {
+        width : 100%;
+        height : 100%;
+    }
+
+    body {
+        background-image: src("./1138.jpg");
+    }
+    
+`;
